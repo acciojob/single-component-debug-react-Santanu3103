4a41b52b1890aa3b68bbb708fa7fc403df208456
santunu <santanuroy253396@gmail.com>
2023-01-14 17:10:23 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import '..styles/App.css';
 
+function App() {
+
+  return (
+    <React.Fragment>
+     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </React.Fragment> 
+  )
+}
